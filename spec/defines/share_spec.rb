93fba282b107ec3835cfdfcd2ea180d2a4e94693
Rehan Mahmood <rehanone@gmail.com>
2022-05-25 06:26:51 -0400

    - Fix tests

--- spec/defines/share_spec.rb
+++ spec/defines/share_spec.rb
@@ -43,7 +43,7 @@ describe 'samba::share' do
         }
 
         it {
-          is_expected.to have_samba__option_resource_count(54)
+          is_expected.to have_samba__option_resource_count(56)
         }
       end
     end
