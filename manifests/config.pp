b3492342d58f72818ea85ea77bb9a7df9a261cde
Hunter Welch <welch214@umn.edu>
2022-07-21 11:49:26 -0500

    Adding machine password timeout parameter

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,33 +12,34 @@ class samba::config () inherits samba {
   }
 
   samba::option {
-    'workgroup':            value => $samba::workgroup;
-    'server string':        value => $samba::server_string;
-    'netbios name':         value => $samba::netbios_name;
-    'domain master':        value => $samba::domain_master;
-    'preferred master':     value => $samba::preferred_master;
-    'local master':         value => $samba::local_master;
-    'os level':             value => $samba::os_level;
-    'wins support':         value => $samba::wins_support;
-    'wins server':          value => $samba::wins_server;
-    'name resolve order':   value => $samba::name_resolve_order;
-    'server min protocol':  value => $samba::server_min_protocol;
-    'client max protocol':  value => $samba::client_max_protocol;
-    'client min protocol':  value => $samba::client_min_protocol;
-    'hosts allow':          value => $samba::hosts_allow;
-    'hosts deny':           value => $samba::hosts_deny;
-    'interfaces':           value => $samba::interfaces;
-    'bind interfaces only': value => $samba::bind_interfaces_only;
-    'security':             value => $samba::security;
-    'encrypt passwords':    value => $samba::encrypt_passwords;
-    'unix password sync':   value => $samba::unix_password_sync;
-    'socket options':       value => $samba::socket_options;
-    'map to guest':         value => $samba::map_to_guest;
-    'passdb backend':       value => $samba::passdb_backend;
-    'log file':             value => $samba::log_file;
-    'max log size':         value => $samba::max_log_size;
-    'syslog':               value => $samba::syslog;
-    'ntlm auth':            value => $samba::ntlm_auth;
+    'workgroup':                value => $samba::workgroup;
+    'server string':            value => $samba::server_string;
+    'netbios name':             value => $samba::netbios_name;
+    'domain master':            value => $samba::domain_master;
+    'preferred master':         value => $samba::preferred_master;
+    'local master':             value => $samba::local_master;
+    'os level':                 value => $samba::os_level;
+    'wins support':             value => $samba::wins_support;
+    'wins server':              value => $samba::wins_server;
+    'name resolve order':       value => $samba::name_resolve_order;
+    'server min protocol':      value => $samba::server_min_protocol;
+    'client max protocol':      value => $samba::client_max_protocol;
+    'client min protocol':      value => $samba::client_min_protocol;
+    'hosts allow':              value => $samba::hosts_allow;
+    'hosts deny':               value => $samba::hosts_deny;
+    'interfaces':               value => $samba::interfaces;
+    'bind interfaces only':     value => $samba::bind_interfaces_only;
+    'security':                 value => $samba::security;
+    'encrypt passwords':        value => $samba::encrypt_passwords;
+    'unix password sync':       value => $samba::unix_password_sync;
+    'socket options':           value => $samba::socket_options;
+    'map to guest':             value => $samba::map_to_guest;
+    'passdb backend':           value => $samba::passdb_backend;
+    'log file':                 value => $samba::log_file;
+    'max log size':             value => $samba::max_log_size;
+    'syslog':                   value => $samba::syslog;
+    'ntlm auth':                value => $samba::ntlm_auth;
+    'machine password timeout': value => $samba::machine_password_timeout;
   }
 
   create_resources('samba::share', $samba::shares)
