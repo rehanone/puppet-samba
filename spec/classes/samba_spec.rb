9b3b0b685468c7937025ed02f54fa6b694889957
Hunter Welch <welch214@umn.edu>
2022-07-27 11:42:59 -0500

    Adding maching password timeout parameter
    Adding machine password timeout parameter
    
    Adding machine_password_timeout to common.yaml
    
    Updating resource count
    
    Trying null

--- spec/classes/samba_spec.rb
+++ spec/classes/samba_spec.rb
@@ -87,10 +87,11 @@ describe 'samba' do
           is_expected.to contain_samba__option('max log size').with_value(10_000)
           is_expected.to contain_samba__option('syslog').with_value(nil)
           is_expected.to contain_samba__option('ntlm auth').with_value(false)
+          is_expected.to contain_samba__option('machine_password_timeout').with_value(nil)
         }
 
         it {
-          is_expected.to have_samba__option_resource_count(27)
+          is_expected.to have_samba__option_resource_count(28)
         }
       end
 
