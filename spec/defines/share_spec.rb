eb7793d36438706f65e89572ac9b2b88232fc229
Hunter Welch <welch214@umn.edu>
2022-12-16 11:14:39 -0600

    Adding obey pam restritctions param

--- spec/defines/share_spec.rb
+++ spec/defines/share_spec.rb
@@ -43,7 +43,7 @@ describe 'samba::share' do
         }
 
         it {
-          is_expected.to have_samba__option_resource_count(62)
+          is_expected.to have_samba__option_resource_count(63)
         }
       end
     end
