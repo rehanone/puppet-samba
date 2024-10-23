11884c1473f2e4e1d0f747a6aeb04e3c1578c801
Doninelli Davide <Davide.Doninelli@eoc.ch>
2024-10-23 10:19:53 +0200

    install only if not already defined
    single space

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,12 +4,15 @@ class samba::install () inherits samba {
   assert_private("Use of private class ${name} by ${caller_module_name}")
 
   if $samba::package_manage {
-    package { $samba::packages[server]:
-      ensure => $samba::package_ensure,
+    if !defined(Package[$samba::packages[server]]) {
+      package { $samba::packages[server]:
+        ensure => $samba::package_ensure,
+      }
     }
-
-    package { $samba::packages[utils]:
-      ensure => $samba::package_ensure,
+    if !defined(Package[$samba::packages[utils]]) {
+      package { $samba::packages[utils]:
+        ensure => $samba::package_ensure,
+      }
     }
   }
 }
